2c22be44ecf375699e7fc1f7c1f5a3550dce2ac2
AntoniiP <75284544+AntoniiP@users.noreply.github.com>
2024-01-09 12:59:06 -0600

    Update auto.js
    Fixed indentation and removed unnecessary async.

--- JS/auto.js
+++ JS/auto.js
@@ -2,28 +2,23 @@ import { GlobalKeyboardListener } from "node-global-key-listener";
 import { mouse, Button } from '@nut-tree/nut-js';
 
 const v = new GlobalKeyboardListener();
-let clicking = false;
-let clickInterval;
+let clicking = false, clickInterval;
 
 console.log('Started listening.');
 
-v.addListener(async (e, down) => {
+v.addListener((e, down) => {
     if (e.name == 'NUMPAD PLUS' && e.state == "DOWN") {
-        console.log('numpad plus pressed.');
-		
-		clicking = !clicking;
-
-        if (clicking) {
+        console.log('Numpad Plus pressed.');
+	clicking = !clicking;
+        
+	if (clicking) {
             console.log('Starting to click...');
             clickInterval = setInterval(async () => {
                 await mouse.click(Button.LEFT);
-            }, 28);
-			
+            }, 28);	
         } else {
-            
-			console.log('Stopped clicking.');
+	    console.log('Stopped clicking.');
             if (clickInterval) clearInterval(clickInterval);
-            
         }
     }
 });
